d2ee61fec39c65f258093ccd112c1fac1c5df594
Larry Bank <laurencebank@gmail.com>
2017-01-09 15:46:20 -0500

    latest lesson

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 A CS lesson for learning C programming
-This code is meant to demonstrate how to work with files, bitmaps and pointers
-The idea is for the student to take this code as a starting point and enhance it.
+This code is meant to demonstrate how to work with pointers and different
+native data types. It allocates memory for 2 arrays and fills one with
+a set of integer values. The program has several errors and warnings which
+prevent it from working properly. The challenge of this lesson is to
+fix the errors so that the program works correctly.
+
 To compile and run the program type at the command line:
 
 make
 ./cs1
 
-It will create a file in the current directory called test.bmp. This file
-and its data was created entirely in C code. A good next step would be to create
-a 32-bpp color image instead of the 1-bpp image provided.
-
 Written by Larry Bank
-Project started 11/17/2016
+Project updated 1/9/2017
 
--- main.c
+++ main.c
@@ -15,115 +15,38 @@
 #include <time.h>
 #include <math.h>
 
-/* Windows BMP header info */
-unsigned char bmp_header[62] =
-        {0x42,0x4d, /* magic values = 'BM' */
-         0,0,0,0,         /* File size */
-         0,0,0,0,
-         0x3e,0,0,0,0x28,0,0,0, /* offset to pixels */
-         0,0,0,0, /* Xsize */
-         0,0,0,0, /* Ysize - needs to be negative or bitmap will be upside down */
-         1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,       /* number of planes, bits per pel */
-         0,0,0,0,0,0,0,0,0,0,0xff,0xff,0xff};     /* color palette */
-
-#define HEADER_FILESIZE 2
-#define HEADER_XSIZE 18
-#define HEADER_YSIZE 22
-#define HEADER_BPP 28
-
-/****************************************************************************
- *                                                                          *
- *  FUNCTION   : MilliTime()                                                *
- *               returns the relative time in milliseconds                  *
- *                                                                          *
- ****************************************************************************/
-int MilliTime()
-{
-int iTime;
-struct timespec res;
-
-    clock_gettime(CLOCK_MONOTONIC, &res);
-    iTime = 1000*res.tv_sec + res.tv_nsec/1000000;
-
-    return iTime;
-} /* MilliTime() */
-
-// Draw a circle
-// Simple (and slow) method for drawing a circle with 1-bit per pixel
-// An exercise for the reader is to enhance this code for other bit depths
-void DrawCircle(unsigned char *pBits, int iPitch, int iRadius, int iXCenter, int iYCenter, unsigned char ucColor)
-{
-float angle;
-int x, y;
-unsigned char *pDest;
-
-   for (angle=0.0; angle<6.28; angle += 0.01)
-   {
-	x = (int)(cos(angle) * iRadius) + iXCenter;
-	y = (int)(sin(angle) * iRadius) + iYCenter;
-        pDest = &pBits[(y * iPitch) + (x >> 3)];
-	*pDest &= ~(0x80 >> (x & 7)); // set pixel to 0
-        if (ucColor) // if non-zero color, set pixel to 1
-        {
-           *pDest |= (0x80 >> (x & 7));           
-	}
-   }
-} /* DrawCircle() */
-
-// Invert a pixel in a 1-bpp bitmap
-void invert_pixel(unsigned char *pBitmap, int iPitch, int x, int y)
-{
-unsigned char *pDest;
-
-	pDest = &pBitmap[(y * iPitch) + (x >> 3)];
-	*pDest ^= (0x80 >> (x & 7)); // XOR the selected pixel with 1
-} /* invert_pixel() */
+#define ARRAY_SIZE 128
 
 int main(int argc, char* argv[])
 {
-FILE *fhandle;
-int i, x, y;
-int iTime;
-unsigned char *pBitmap;
+int i, j;
+uint32_t *pInt32s;
+uint8_t *pInt8s
 
-// Create a 256 x 256 bitmap (1 bit per pixel)
-   pBitmap = malloc(256 * 32); // 256 lines x 32 bytes per line
-   memset(pBitmap, 0xf0, 256*32); // fill it with white and black stripes
-// Draw a black circle in the center
-   DrawCircle(pBitmap, 256>>3, 50, 128, 128, 0);
+// Allocate memory for 2 arrays of integers
+   pInt32s = malloc(ARRAY_SIZE * sizeof(uint32_t);
+   pInt8s = malloc(ARRAY_SIZE * sizeof(uint8_t);
 
-//
-// Measure the time it takes to invert the bitmap 999 times
-// Do it an odd number of times so that it comes out inverted
-//
-   iTime = MilliTime();
-   for (i=0; i<999; i++)
+   for (i=0; i<ARRAY_SIZE; i++)
    {
-      for(x=0; x<256; x++) // go through every pixel and invert it
-      {
-         for (y=0; y<256; y++)
-         {
-            invert_pixel(pBitmap, 32, x, y);
-         }
-      }
+      pInt32s[i] = i; // fill the array such that each element has the value of its index
    }
-   iTime = MilliTime() - iTime;
-   printf("Time to invert the bitmap 999 times = %d milliseconds\n", iTime);
 
-// Prepare the Windows BMP file header
-   *(int *)&bmp_header[HEADER_FILESIZE] = sizeof(bmp_header) + 256*32;
-   *(int *)&bmp_header[HEADER_XSIZE] = 256;
-   *(int *)&bmp_header[HEADER_YSIZE] = -256; // Windows BMP have Y flipped
-   *(short *)&bmp_header[HEADER_BPP] = 1; // 1 bit per pixel
-
-// Create the output file
-   fhandle = fopen("test.bmp","w+");
-   if (fhandle != NULL)
+   // convert the values from uint32_t's into uint8_t's with direct pointer access
+   j = 0;
+   for (i=0; i<ARRAY_SIZE; i++)
    {
-      fwrite(bmp_header, 1, sizeof(bmp_header), fhandle); // Write the BMP file header
-      fwrite(pBitmap, 1, 256*32, fhandle); // write the pixels
-      fclose(fhandle);
+      *pInt8s = *pIn32s;
+      pInt32s += 4;
+      pIn8s += 1;
    }
-   free(pBitmap); // free the memory for the bitmap
+   free(pInt32s);
+   free(pInt8s);
+
+   for (i=0; i<ARRAY_SIZE; i++)
+   {
+      printf("pInt8s[%d] = %d\n", pInts8[i]);
+   }
+
    return 0;
 } /* main() */
